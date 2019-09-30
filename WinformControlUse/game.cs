bd944ea4aadbd99e52074cc23c7a67c4173d469f
cl12312 <943853245@qq.com>
2019-09-30 14:52:52 +0800

    201731024130

---
+++ WinformControlUse/game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformControlUse
+{
+    public partial class game : Form
+    {
+        public game()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"C:/Users/chen123/Desktop/材料/timg.gif");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            Random rd = new Random();
+            int i = rd.Next(0, 5);
+            pictureBox1.Image = imageList1.Images[i];
+        }
+    }
+}
---
+++ WinformControlUse/login.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WinformControlUse;
+
+
+namespace WiControl
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = this.textBox1.Text;
+            string password = this.textBox2.Text;
+            if(name.Equals("admin") && password .Equals ("123"))
+            {
+                MessageBox.Show("登录成功");
+                this.Hide();
+                welcome w = new welcome();
+                w.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("密码或账号错误！");
+                this.textBox1.Clear();
+                this.textBox2.Clear();
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+            this.textBox2.Clear();
+        }
+    }
+}
